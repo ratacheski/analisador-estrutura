30f9c74c08291d8fe397e07bd735ca1f2fe466a6
Rafael Ratacheski <rafaelratacheski@gmail.com>
2021-05-16 15:51:10 -0300

    Ajuste das unidades de medida e arredondamento para o relatório

--- components/Relatorio.vue
+++ components/Relatorio.vue
@@ -17,62 +17,75 @@
           <v-list-item-icon>
             <v-icon>mdi-weight-lifter</v-icon>
           </v-list-item-icon>
-          <v-list-item-subtitle>Va = {{ dados.vA }}</v-list-item-subtitle>
+          <v-list-item-subtitle class="text-left"
+            >Va = {{ calcularString('vA') }}</v-list-item-subtitle
+          >
         </v-list-item>
         <v-list-item>
           <v-list-item-icon>
             <v-icon>mdi-weight-lifter</v-icon>
           </v-list-item-icon>
-          <v-list-item-subtitle>Vb = {{ dados.vB }}</v-list-item-subtitle>
+          <v-list-item-subtitle class="text-left"
+            >Vb = {{ calcularString('vB') }}</v-list-item-subtitle
+          >
         </v-list-item>
         <v-list-item>
           <v-list-item-icon>
             <v-icon>mdi-weight-lifter</v-icon>
           </v-list-item-icon>
-          <v-list-item-subtitle>Vd = {{ dados.vD }}</v-list-item-subtitle>
+          <v-list-item-subtitle class="text-left"
+            >Vd = {{ calcularString('vD') }}</v-list-item-subtitle
+          >
         </v-list-item>
       </div>
       <v-divider :vertical="!$vuetify.breakpoint.mobile" />
       <div>
-        <h3 class="mt-2">Deformações</h3>
+        <h3 class="mt-2 mt-md-0">Deformações</h3>
         <v-list-item class="">
           <v-list-item-icon>
             <v-icon>mdi-scale</v-icon>
           </v-list-item-icon>
-          <v-list-item-subtitle>Va = {{ dados.deformBC }}</v-list-item-subtitle>
+          <v-list-item-subtitle class="text-left"
+            >Va = {{ calcularString('deformBC') }}</v-list-item-subtitle
+          >
         </v-list-item>
         <v-list-item>
           <v-list-item-icon>
             <v-icon>mdi-scale</v-icon>
           </v-list-item-icon>
-          <v-list-item-subtitle>Vb = {{ dados.deformDE }}</v-list-item-subtitle>
+          <v-list-item-subtitle class="text-left"
+            >Vb = {{ calcularString('deformDE') }}</v-list-item-subtitle
+          >
         </v-list-item>
       </div>
       <v-divider :vertical="!$vuetify.breakpoint.mobile" />
       <div>
-        <h3 class="mt-2">Deslocamentos</h3>
+        <h3 class="mt-2 mt-md-0">Deslocamentos</h3>
         <v-list-item>
           <v-list-item-icon>
             <v-icon>mdi-map-marker</v-icon>
           </v-list-item-icon>
-          <v-list-item-subtitle
-            >δ<sub>C</sub> = {{ dados.deslocC }}</v-list-item-subtitle
+          <v-list-item-subtitle class="text-left"
+            >δ<sub>C</sub> =
+            {{ calcularString('deslocC') }}</v-list-item-subtitle
           >
         </v-list-item>
         <v-list-item>
           <v-list-item-icon>
             <v-icon>mdi-map-marker</v-icon>
           </v-list-item-icon>
-          <v-list-item-subtitle
-            >δ<sub>E</sub> = {{ dados.deslocE }}</v-list-item-subtitle
+          <v-list-item-subtitle class="text-left"
+            >δ<sub>E</sub> =
+            {{ calcularString('deslocE') }}</v-list-item-subtitle
           >
         </v-list-item>
         <v-list-item>
           <v-list-item-icon>
             <v-icon>mdi-map-marker</v-icon>
           </v-list-item-icon>
-          <v-list-item-subtitle
-            >δ<sub>F</sub> = {{ dados.deslocF }}</v-list-item-subtitle
+          <v-list-item-subtitle class="text-left"
+            >δ<sub>F</sub> =
+            {{ calcularString('deslocF') }}</v-list-item-subtitle
           >
         </v-list-item>
       </div>
@@ -88,9 +101,58 @@ export default {
       default: null,
     },
   },
+  data() {
+    return {
+      unidadeMedida: {
+        vA: 'N',
+        vB: 'N',
+        vD: 'N',
+        deformBC: 'm',
+        deformDE: 'm',
+        deslocC: 'm',
+        deslocE: 'm',
+        deslocF: 'm',
+      },
+    }
+  },
   methods: {
     onClickOutside() {
       this.$emit('close')
+    },
+    calcularString(valor) {
+      const valorCheio = this.dados[valor]
+      if (Math.abs(valorCheio) >= 0.8e9) {
+        return (
+          this.roundValue(valorCheio / 1e9) + ' G' + this.unidadeMedida[valor]
+        )
+      } else if (Math.abs(valorCheio) >= 0.8e6) {
+        return (
+          this.roundValue(valorCheio / 1e6) + ' M' + this.unidadeMedida[valor]
+        )
+      } else if (Math.abs(valorCheio) >= 0.8e3) {
+        return (
+          this.roundValue(valorCheio / 1e3) + ' K' + this.unidadeMedida[valor]
+        )
+      } else if (Math.abs(valorCheio) >= 0.8) {
+        return this.roundValue(valorCheio) + ' ' + this.unidadeMedida[valor]
+      } else if (Math.abs(valorCheio) >= 0.8e-3) {
+        return (
+          this.roundValue(valorCheio / 1e-3) + ' m' + this.unidadeMedida[valor]
+        )
+      } else if (Math.abs(valorCheio) >= 0.8e-6) {
+        return (
+          this.roundValue(valorCheio / 1e-6) + ' µ' + this.unidadeMedida[valor]
+        )
+      } else if (Math.abs(valorCheio) >= 0.8e-9) {
+        return (
+          this.roundValue(valorCheio / 1e-9) + ' n' + this.unidadeMedida[valor]
+        )
+      } else {
+        return this.roundValue(valorCheio) + ' ' + this.unidadeMedida[valor]
+      }
+    },
+    roundValue(value) {
+      return Math.round(value * 100) / 100
     },
   },
 }
